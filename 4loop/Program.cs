155b163fc90f207c12d8798dbc0c5aa1f2a0972b
KasparSiivelt5 <89830554+KasparSiivelt5@users.noreply.github.com>
2023-05-17 22:31:16 +0300

    Add files via upload

---
+++ 4loop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            PrintHello();
+        }
+    }
+
+    static void PrintHello()
+    {
+        Console.WriteLine("Hello world");
+    }
+}
